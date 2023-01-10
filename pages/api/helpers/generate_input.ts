fb06581723fc01c410d909926cd26c57d58a2927
Sehyun Chung <sehyun@berkeley.edu>
2023-01-09 22:22:05 -0800

    update generate helpers

--- pages/api/helpers/generate_input.ts
+++ pages/api/helpers/generate_input.ts
@@ -6,6 +6,31 @@ import { MAX_HEADER_PADDED_BYTES } from "./constants";
 import { Hash } from "./fast-sha256";
 const pki = require("node-forge").pki;
 
+export async function generate_inputs(
+  signature: string = "mLCysHQtDftfFey4F-ntFma22r5-qpxtkXsiDw6TY30Tnoj2kPQ_YdSjzagrwRgF7pHE8SSM_roo2wDh3c_8vDNRZeax4VICZjYmPS-3ZWAV0XyjjlgWgFconstqVT72M-VlPCdecHiYQJojlYHJyGybvTCaX1cqoF9aAMy8wBvRbSceECmX15k4nKG51Z5Le7k_vOShaxYmwrRhMIip4KRv-DW1FXAdi_F-MYSrqZ6Oq-nglMujxD2NOoHoqOqmyd1OMIrc6oIRuRqBXlRnQ0IdUDQbiXfyFVC0ItIME3a4SLoWp_rrmY1tSrGJu93MZrjhzfkNglJ-FOp4kKZAKkzA",
+  msg: string = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJzZWh5dW5AYmVya2VsZXkuZWR1IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImdlb2lwX2NvdW50cnkiOiJVUyJ9LCJodHRwczovL2FwaS5vcGVuYWkuY29tL2F1dGgiOnsidXNlcl9pZCI6InVzZXIta1dMaXBzT3dMZFd4MXdMc0I3clR3UnFlIn0sImlzcyI6Imh0dHBzOi8vYXV0aDAub3BlbmFpLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExNjYwOTg2MjEwMzkxMTMwNjgwNyIsImF1ZCI6WyJodHRwczovL2FwaS5vcGVuYWkuY29tL3YxIiwiaHR0cHM6Ly9vcGVuYWkuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY3MzE1NTQ0NiwiZXhwIjoxNjczNzYwMjQ2LCJhenAiOiJUZEpJY2JlMTZXb1RIdE45NW55eXdoNUU0eU9vNkl0RyIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgbW9kZWwucmVhZCBtb2RlbC5yZXF1ZXN0IG9yZ2FuaXphdGlvbi5yZWFkIG9mZmxpbmVfYWNjZXNzIn0",
+  ethAddress: string = "0x0000000000000000000000000000000000000000"
+): Promise<any> {
+  const sig = BigInt("0x" + Buffer.from(signature, "base64").toString("hex"));
+  const message = Buffer.from(msg);
+  const circuitType = CircuitType.JWT;
+  const pubkey = fs.readFileSync(
+    "/Users/sehyun/code/zk-blind-www/pages/api/helpers/public_key.pem"
+  );
+  const pubKeyData = pki.publicKeyFromPem(pubkey.toString());
+
+  const modulus = BigInt(pubKeyData.n.toString());
+  const fin_result = await getCircuitInputs(
+    sig,
+    modulus,
+    message,
+    ethAddress,
+    circuitType
+  );
+
+  return fin_result.circuitInputs;
+}
+
 export interface ICircuitInputs {
   message?: string[];
   modulus?: string[];
@@ -29,16 +54,16 @@ function assert(cond: boolean, errorMessage: string) {
 
 // Works only on 32 bit sha text lengths
 function int32toBytes(num: number): Uint8Array {
-  let arr = new ArrayBuffer(4); // an Int32 takes 4 bytes
-  let view = new DataView(arr);
+  const arr = new ArrayBuffer(4); // an Int32 takes 4 bytes
+  const view = new DataView(arr);
   view.setUint32(0, num, false); // byteOffset = 0; litteEndian = false
   return new Uint8Array(arr);
 }
 
 // Works only on 32 bit sha text lengths
 function int8toBytes(num: number): Uint8Array {
-  let arr = new ArrayBuffer(1); // an Int8 takes 4 bytes
-  let view = new DataView(arr);
+  const arr = new ArrayBuffer(1); // an Int8 takes 4 bytes
+  const view = new DataView(arr);
   view.setUint8(0, num); // byteOffset = 0; litteEndian = false
   return new Uint8Array(arr);
 }
@@ -56,8 +81,8 @@ async function sha256Pad(
   prehash_prepad_m: Uint8Array,
   maxShaBytes: number
 ): Promise<[Uint8Array, number]> {
-  let length_bits = prehash_prepad_m.length * 8; // bytes to bits
-  let length_in_bytes = int32toBytes(length_bits);
+  const length_bits = prehash_prepad_m.length * 8; // bytes to bits
+  const length_in_bytes = int32toBytes(length_bits);
   prehash_prepad_m = mergeUInt8Arrays(prehash_prepad_m, int8toBytes(2 ** 7));
   while (
     (prehash_prepad_m.length * 8 + length_in_bytes.length * 8) % 512 !==
@@ -68,15 +93,15 @@ async function sha256Pad(
   prehash_prepad_m = mergeUInt8Arrays(prehash_prepad_m, length_in_bytes);
   assert(
     (prehash_prepad_m.length * 8) % 512 === 0,
-    "Padding did not complete properly!"
+    "Padding did not compconste properly!"
   );
-  let messageLen = prehash_prepad_m.length;
+  const messageLen = prehash_prepad_m.length;
   while (prehash_prepad_m.length < maxShaBytes) {
     prehash_prepad_m = mergeUInt8Arrays(prehash_prepad_m, int32toBytes(0));
   }
   assert(
     prehash_prepad_m.length === maxShaBytes,
-    "Padding to max length did not complete properly!"
+    "Padding to max length did not compconste properly!"
   );
 
   return [prehash_prepad_m, messageLen];
@@ -142,7 +167,6 @@ export async function getCircuitInputs(
   );
 
   // Compute identity revealer
-  let circuitInputs;
   const modulus = toCircomBigIntBytes(modulusBigInt);
   const signature = toCircomBigIntBytes(signatureBigInt);
 
@@ -154,7 +178,7 @@ export async function getCircuitInputs(
     bytesToBigInt(fromHex(eth_address)) + 1n
   ).toString();
 
-  circuitInputs = {
+  const circuitInputs = {
     message,
     modulus,
     signature,
@@ -166,37 +190,4 @@ export async function getCircuitInputs(
     circuitInputs,
     valid: {},
   };
-}
-
-export async function generate_inputs(): Promise<any> {
-  const signature =
-    "mLCysHQtDftfFey4F-ntFma22r5-qpxtkXsiDw6TY30Tnoj2kPQ_YdSjzagrwRgF7pHE8SSM_roo2wDh3c_8vDNRZeax4VICZjYmPS-3ZWAV0XyjjlgWgFleTqVT72M-VlPCdecHiYQJojlYHJyGybvTCaX1cqoF9aAMy8wBvRbSceECmX15k4nKG51Z5Le7k_vOShaxYmwrRhMIip4KRv-DW1FXAdi_F-MYSrqZ6Oq-nglMujxD2NOoHoqOqmyd1OMIrc6oIRuRqBXlRnQ0IdUDQbiXfyFVC0ItIME3a4SLoWp_rrmY1tSrGJu93MZrjhzfkNglJ-FOp4kKZAKkzA";
-
-  let sig = BigInt("0x" + Buffer.from(signature, "base64").toString("hex"));
-
-  console.log("decoded sig");
-  console.log(sig);
-
-  let msg =
-    "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJzZWh5dW5AYmVya2VsZXkuZWR1IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImdlb2lwX2NvdW50cnkiOiJVUyJ9LCJodHRwczovL2FwaS5vcGVuYWkuY29tL2F1dGgiOnsidXNlcl9pZCI6InVzZXIta1dMaXBzT3dMZFd4MXdMc0I3clR3UnFlIn0sImlzcyI6Imh0dHBzOi8vYXV0aDAub3BlbmFpLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExNjYwOTg2MjEwMzkxMTMwNjgwNyIsImF1ZCI6WyJodHRwczovL2FwaS5vcGVuYWkuY29tL3YxIiwiaHR0cHM6Ly9vcGVuYWkuYXV0aDAuY29tL3VzZXJpbmZvIl0sImlhdCI6MTY3MzE1NTQ0NiwiZXhwIjoxNjczNzYwMjQ2LCJhenAiOiJUZEpJY2JlMTZXb1RIdE45NW55eXdoNUU0eU9vNkl0RyIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUgZW1haWwgbW9kZWwucmVhZCBtb2RlbC5yZXF1ZXN0IG9yZ2FuaXphdGlvbi5yZWFkIG9mZmxpbmVfYWNjZXNzIn0";
-  let message = Buffer.from(msg);
-  let circuitType = CircuitType.JWT;
-  // let pubkey = fs.readFileSync("./public_key.pem", {
-  let pubkey = fs.readFileSync(
-    "/Users/sehyun/code/zk-blind-www/pages/api/helpers/public_key.pem"
-  );
-  const pubKeyData = pki.publicKeyFromPem(pubkey.toString());
-
-  const eth_address = "0x0000000000000000000000000000000000000000";
-
-  let modulus = BigInt(pubKeyData.n.toString());
-  let fin_result = await getCircuitInputs(
-    sig,
-    modulus,
-    message,
-    eth_address,
-    circuitType
-  );
-
-  return fin_result.circuitInputs;
 }
