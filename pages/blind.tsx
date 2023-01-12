d4431ab2aa6f4d176f60848b30d72627fc25c332
Sehyun Chung <sehyun@berkeley.edu>
2023-01-12 15:05:27 -0800

    add form to blind

--- pages/blind.tsx
+++ pages/blind.tsx
@@ -1,27 +1,70 @@
+import {
+  Button,
+  Container,
+  Flex,
+  Input,
+  Text,
+  Textarea
+} from '@chakra-ui/react'
+import { Silkscreen } from '@next/font/google'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { useAccount, useContractRead, useWaitForTransaction } from 'wagmi'
+import { useEffect, useState } from 'react'
+import { useAccount, useContractRead } from 'wagmi'
 import { abi } from '../constants/abi'
-import { useState } from 'react'
 
-type Props = {}
+const font = Silkscreen({ subsets: ['latin'], weight: '400' })
 
-const Blind = (props: Props) => {
+const Blind = () => {
+  const [title, setTitle] = useState('')
+  const [message, setMessage] = useState('')
   const { address } = useAccount()
   const formattedAddr = address ? address : '0x'
   const [enabled, setEnabled] = useState(false)
   const { data: domainStr } = useContractRead({
-    address: '0x04dc2484cc09c2E1c7496111A18b30878b7d14B2',
+    address: enabled ? '0x04dc2484cc09c2E1c7496111A18b30878b7d14B2' : '0x',
     abi,
     functionName: 'get',
-    args: [formattedAddr],
-    enabled
+    args: [formattedAddr]
   })
-  console.log('🚀 ~ Blind ~ domainStr', domainStr)
+
+  useEffect(() => {
+    if (!enabled) setEnabled(true)
+  }, [enabled])
+
   return (
-    <div>
-      <ConnectButton />
-      <p>{domainStr}</p>
-    </div>
+    <Container
+      as={Flex}
+      centerContent
+      gap="6"
+      justifyContent="center"
+      minH="100vh"
+    >
+      <Flex
+        direction="column"
+        alignItems="center"
+        backgroundColor="#241520"
+        padding="8"
+        gap="4"
+        borderRadius="10"
+      >
+        <div>
+          <ConnectButton />
+        </div>
+        <div>
+          <Text>Title</Text>
+          <Input onChange={e => setTitle(e.target.value)} value={title} />
+        </div>
+        <div>
+          <Text>Post</Text>
+          <Textarea
+            onChange={e => setMessage(e.target.value)}
+            value={message}
+          />
+        </div>
+        <div className={font.className}>Committed to {domainStr}</div>
+        <Button>Create Post</Button>
+      </Flex>
+    </Container>
   )
 }
 
