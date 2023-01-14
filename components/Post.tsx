7c7c864b6167a68b8515d741867340c07a0e7caf
Sehyun Chung <sehyun@berkeley.edu>
2023-01-13 21:14:23 -0800

    verify posts works

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,5 +1,8 @@
-import { Box, Container, Flex, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Text, useToast } from '@chakra-ui/react'
 import { Karla, Silkscreen } from '@next/font/google'
+import { ethers } from 'ethers'
+import { useContract, useSigner } from 'wagmi'
+import { abi } from '../constants/abi'
 
 const font = Silkscreen({ subsets: ['latin'], weight: '400' })
 const bodyFont = Karla({ weight: '300' })
@@ -14,6 +17,39 @@ const Post = ({
   signature: string
   company: string
 }) => {
+  const toast = useToast()
+  const { data: signer } = useSigner()
+  const sig = ethers.utils.splitSignature(signature as any)
+  const signingAddr = ethers.utils.verifyMessage(msg, sig)
+  const blind = useContract({
+    address: '0x13e4E0a14729d9b7017E77ebbDEad05cb8ad1540',
+    abi,
+    signerOrProvider: signer
+  })
+  async function verifySig() {
+    if (!blind || !signingAddr) return
+    const domain = await blind.get(signingAddr as `0x${string}`)
+    if (domain) {
+      console.log('verified')
+      toast({
+        title: 'Message verified.',
+        description: "We've verified the sender's signature for you",
+        status: 'success',
+        duration: 9000,
+        isClosable: true
+      })
+    } else {
+      console.log('not verified')
+      toast({
+        title: 'Message not verified.',
+        description: 'This signer is not a valid poster.',
+        status: 'error',
+        duration: 9000,
+        isClosable: true
+      })
+    }
+    // verify that the signingaddr is the same as the addr we want
+  }
   return (
     <>
       <Flex
@@ -36,27 +72,25 @@ const Post = ({
         }}
       >
         <Flex direction="row" justifyContent="space-between" w="100%">
-          <div>
-            <Box
-              backgroundColor="#4C82FB"
-              className={font.className}
-              borderRadius="4"
-              px="3"
-              style={{ textTransform: 'capitalize' }}
-            >
-              {company}
-            </Box>
-          </div>
-          <>
-            <Text className={bodyFont.className}>
-              Signature:{' '}
-              {`${signature?.substring(0, 5)}...${signature?.substring(
-                signature.length - 5
-              )}`}
-            </Text>
-          </>
+          <Box
+            backgroundColor="#4C82FB"
+            className={font.className}
+            borderRadius="4"
+            px="3"
+            style={{ textTransform: 'capitalize' }}
+          >
+            {company}
+          </Box>
+          <Text className={bodyFont.className}>
+            Signature:{' '}
+            {`${signature?.substring(0, 5)}...${signature?.substring(
+              signature.length - 5
+            )}`}
+          </Text>
+          <Button onClick={verifySig} variant="link">
+            Verify
+          </Button>
         </Flex>
-
         <Text
           alignContent="start"
           display="block"
